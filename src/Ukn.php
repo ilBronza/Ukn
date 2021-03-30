998fdd8e06495d319feb3dd363f6e38a55fa3c27
Davide Bronzini <davide@sistema.srl>
2021-03-31 00:10:09 +0200

    Initial commit

---
+++ src/Ukn.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ilBronza\Ukn;
+
+use Illuminate\Support\Str;
+
+class UKN
+{
+	static function displayDatabaseNotification(array $notificationFields)
+	{
+		static::w(json_encode($notificationFields));
+	}
+
+	static function e(string $message = null)
+	{
+		static::error($message);
+	}
+
+	static function s(string $message = null)
+	{
+		static::success($message);
+	}
+
+	static function w(string $message = null)
+	{
+		static::warning($message);
+	}
+
+	static function i(string $message = null)
+	{
+		static::info($message);
+	}
+
+	static function pushUikitNotification(string $type, string $message)
+	{
+		$key = [
+			'UiKitNotifications',
+			$type,
+			Str::random()
+		];
+
+		session()->flash(implode(".", $key) , $message ?? __('messages.' . $type));
+	}
+
+	static function error(string $message)
+	{
+		static::pushUikitNotification('danger', $message);
+	}
+
+	static function success(string $message)
+	{
+		static::pushUikitNotification('success', $message);
+	}
+
+	static function warning(string $message)
+	{
+		static::pushUikitNotification('warning', $message);
+	}
+
+	static function info(string $message)
+	{
+		static::pushUikitNotification('primary', $message);
+	}
+}
